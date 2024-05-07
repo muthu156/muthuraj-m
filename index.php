164977d0863a7b5bc5399071d9f8faf211ba38fc
muthu156 <156213996+muthu156@users.noreply.github.com>
2024-05-07 18:30:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+ /*$servername="localhost";
+ $username="root";
+ $password="";
+ $database_name="muthuconstruction";
+ $conn=mysqli_connect($servername,$username,$password,$database_name);
+ if($conn){
+    die("connection failed:".mysqli_connect_error());
+ }
+ if(isset($_POST['save'])){
+    $name=$_POST['name'];
+    $pno=$_POST['pno'];
+    $gen=$_POST['gen'];
+
+    $lady=$_POST['lady'];
+    $address=$_POST['address'];
+    $sql_query="INSERT INTO customerdata("
+ }*/
+ if(isset($_POST['save'])){
+ echo "super da"
+ }
+
+
+?>
+
+
